20cb4e07040eed9690a06940adbc8a5af244739e
matiasnnr <matias.nnr@hotmail.com>
2020-12-23 09:49:31 -0300

    add firebase hosting

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-const Map = () => {
+const Map = ({ data }) => {
 
     const mapStyles = {
         height: "50vh",
@@ -9,8 +9,8 @@ const Map = () => {
     }
 
     const defaultCenter = {
-        lat: 19.4267261,
-        lng: -99.1718796
+        lat: data.lat,
+        lng: data.lng
     }
 
     return (
